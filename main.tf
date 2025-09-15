51072c2f8674ef95e6a26819662c3e68333bc3c6
Harry <harinther2000@gmail.com>
2025-09-15 13:52:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -111,9 +111,9 @@ resource "aws_eks_node_group" "devops" {
   subnet_ids      = aws_subnet.devops[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 
   instance_types = ["t2.medium"]
