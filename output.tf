c5258940288839eb33546b6f3b39ca75ad0d6889
Harry <harinther2000@gmail.com>
2025-09-15 12:54:39 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,10 +7,9 @@ output "node_group_id" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.devops_vpc.id
+  value = aws_vpc.devops.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devops_subnet[*].id
+  value = aws_subnet.devops[*].id
 }
-
